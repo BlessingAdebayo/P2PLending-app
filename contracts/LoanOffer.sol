6b56cef4d4fdc7f7d1b68088b414a62be6f140fb
BlessingAdebayo <adebayoblessing407@gmail.com>
2023-01-11 18:51:58 +0100

    Version upgrade

--- contracts/LoanOffer.sol
+++ contracts/LoanOffer.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract LoanOffer {
   using SafeMath for uint256;
 
+  // Data structure for storing the details of a loan offer
   struct LoanOfferData {
     uint256 id;
     address borrower;
@@ -21,20 +22,25 @@ contract LoanOffer {
     uint256 remainingBalance;
   }
 
+  // Mapping from loan offer ID to loan offer data
   mapping(uint256 => LoanOfferData) public loanOffers;
+  // ID of the next loan offer
   uint256 public nextLoanOfferId;
 
+  // Creates a new loan offer
   function createLoanOffer(address _borrower, address _lender, uint256 _amount, uint256 _interestRate, uint256 _duration) public {
     uint256 id = nextLoanOfferId.add(1);
     loanOffers[id] = LoanOfferData(id, _borrower, _lender, _amount, _amount, _interestRate, _duration, false, false, false, false, false, _amount);
     nextLoanOfferId = id;
   }
 
+    // Returns the data for a specific loan offer
   function viewLoanOffer(uint256 _id) public view returns (uint256, address, address, uint256, uint256, uint256, bool, bool, bool, bool, bool, uint256) {
     LoanOfferData storage loanOffer = loanOffers[_id];
     return (loanOffer.id, loanOffer.borrower, loanOffer.lender, loanOffer.amount, loanOffer.interestRate, loanOffer.duration, loanOffer.isAccepted, loanOffer.repaymentReceived, loanOffer.prepaymentRequested, loanOffer.prepaymentApproved, loanOffer.prepaymentReceived, loanOffer.remainingBalance);
   }
 
+  // Accepts a loan offer
   function acceptLoanOffer(uint256 _id) public {
     // Check that the loan offer with the given ID exists
     require(loanOffers[_id].id != 0, "Loan offer not found");
@@ -45,7 +51,8 @@ contract LoanOffer {
     loanOffers[_id].isAccepted = true;
   }
 
-    function markRepaymentReceived(uint256 _id) public payable {
+  // Marks a repayment as received
+  function markRepaymentReceived(uint256 _id) public payable {
     // Check that the loan offer with the given ID exists
     require(loanOffers[_id].id != 0, "Loan offer not found");
     // Check that the loan offer has been accepted
@@ -62,8 +69,7 @@ contract LoanOffer {
     loanOffers[_id].repaymentReceived = true;
   }
 
-  
-
+    // Requests a prepayment
   function requestPrepayment(uint256 _id) public {
     // Check that the loan offer with the given ID exists
     require(loanOffers[_id].id != 0, "Loan offer not found");
@@ -71,41 +77,42 @@ contract LoanOffer {
     require(loanOffers[_id].isAccepted == true, "Loan offer has not been accepted");
     // Check that the current user is the borrower for the loan offer
     require(loanOffers[_id].borrower == msg.sender, "Only the borrower can request a prepayment");
-    // Check that the borrower has not already requested a prepayment
+    // Check that a prepayment has not already been requested
     require(loanOffers[_id].prepaymentRequested == false, "Prepayment has already been requested");
-    // Mark the prepayment as requested
     loanOffers[_id].prepaymentRequested = true;
   }
 
+  // Approves a prepayment
   function approvePrepayment(uint256 _id) public {
     // Check that the loan offer with the given ID exists
     require(loanOffers[_id].id != 0, "Loan offer not found");
     // Check that the loan offer has been accepted
     require(loanOffers[_id].isAccepted == true, "Loan offer has not been accepted");
+    // Check that a prepayment has been requested
+    require(loanOffers[_id].prepaymentRequested == true, "No prepayment requested");
     // Check that the current user is the lender for the loan offer
     require(loanOffers[_id].lender == msg.sender, "Only the lender can approve a prepayment");
-    // Check that the borrower has requested a prepayment
-    require(loanOffers[_id].prepaymentRequested == true, "Prepayment has not been requested");
-    // Mark the prepayment as approved
     loanOffers[_id].prepaymentApproved = true;
   }
 
-  function makePrepayment(uint256 _id, uint256 _amount) public {
+    // Marks a prepayment as received
+  function markPrepaymentReceived(uint256 _id) public {
     // Check that the loan offer with the given ID exists
     require(loanOffers[_id].id != 0, "Loan offer not found");
     // Check that the loan offer has been accepted
     require(loanOffers[_id].isAccepted == true, "Loan offer has not been accepted");
-    // Check that the current user is the borrower for the loan offer
-    require(loanOffers[_id].borrower == msg.sender, "Only the borrower can make a prepayment");
-    // Check that the borrower has requested a prepayment
-    require(loanOffers[_id].prepaymentRequested == true, "Prepayment has not been requested");
-    // Check that the prepayment has been approved by the lender
+    // Check that a prepayment has been requested
+    require(loanOffers[_id].prepaymentRequested == true, "No prepayment requested");
+    // Check that a prepayment has been approved
     require(loanOffers[_id].prepaymentApproved == true, "Prepayment has not been approved");
-    // Check that the given amount is greater than or equal to the remaining balance of the loan
-    require(_amount >= (loanOffers[_id].remainingBalance), "Invalid prepayment amount");
-    // Update the remaining balance of the loan
-    loanOffers[_id].remainingBalance = loanOffers[_id].remainingBalance.sub(_amount);
-    // Mark the prepayment as received
+    // Check that the current user is the lender for the loan offer
+    require(loanOffers[_id].lender == msg.sender, "Only the lender can mark a prepayment as received");
+    // Check that the prepayment has not already been received
+    require(loanOffers[_id].prepaymentReceived == false, "Prepayment has already been received");
     loanOffers[_id].prepaymentReceived = true;
+    loanOffers[_id].remainingBalance = loanOffers[_id].remainingBalance.sub(loanOffers[_id].remainingBalance);
   }
 }
+
+
+ 
--- contracts/LoanRequest.sol
+++ contracts/LoanRequest.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+// This contract defines the struct and functions for loan requests
 contract LoanRequest {
   using SafeMath for uint256;
-
+  
+  // struct to store the loan request data
   struct LoanRequestData {
     uint256 id;
     address borrower;
@@ -16,20 +18,27 @@ contract LoanRequest {
     uint256 remainingBalance;
   }
 
+  // Mapping to store the loan requests
   mapping(uint256 => LoanRequestData) public loanRequests;
+  // variable to track the next loan request ID
   uint256 public nextLoanRequestId;
 
+  // Function to create a new loan request
   function createLoanRequest(address _borrower, address _lender, uint256 _amount, uint256 _interestRate, uint256 _duration) public {
     uint256 id = nextLoanRequestId.add(1);
+    // Create a new loan request and add it to the mapping
     loanRequests[id] = LoanRequestData(id, _borrower, _lender, _amount, _interestRate, _duration, false, _amount);
     nextLoanRequestId = id;
   }
 
+  // Function to view a loan request
   function viewLoanRequest(uint256 _id) public view returns (uint256, address, address, uint256, uint256, uint256, bool) {
     LoanRequestData storage loanRequest = loanRequests[_id];
+    // Return the details of the loan request
     return (loanRequest.id, loanRequest.borrower, loanRequest.lender, loanRequest.amount, loanRequest.interestRate, loanRequest.duration, loanRequest.isAccepted);
   }
 
+  // Function to accept a loan request
   function acceptLoanRequest(uint256 _id) public {
     // Check that the loan request with the given ID exists
     require(loanRequests[_id].id != 0, "Loan request not found");
@@ -42,12 +51,13 @@ contract LoanRequest {
     loanRequests[_id].isAccepted = true;
   }
 
+  // Function to make a repayment
   function makeRepayment(uint256 _id, uint256 _amount) public {
     // Check that the loan request with the given ID exists
     require(loanRequests[_id].id != 0, "Loan request not found");
     // Check that the loan request has been accepted
     require(loanRequests[_id].isAccepted == true, "Loan request has not been accepted");
-    // Check that the current user is the borrower for the loan request
+        // Check that the current user is the borrower for the loan request
     require(loanRequests[_id].borrower == msg.sender, "Only the borrower can make loan repayments");
     // Check that the repayment amount is greater than 0
     require(_amount > 0, "Invalid repayment amount");
@@ -58,6 +68,7 @@ contract LoanRequest {
     loanRequests[_id].remainingBalance -= _amount;
   }
 
+  // Function to mark a repayment as received
   function markRepaymentReceived(uint256 _id) public view{
     // Check that the loan request with the given ID exists
     require(loanRequests[_id].id != 0, "Loan request not found");
@@ -70,14 +81,16 @@ contract LoanRequest {
     // (You can add code here to update a field in the loanRequest struct to track the status of the repayment)
   }
 
+  // Function to cancel a loan request
   function cancelLoanRequest(uint256 _id) public {
     // Check that the loan request with the given ID exists
     require(loanRequests[_id].id != 0, "Loan request not found");
    // Check that the loan request has not already been accepted
-    require(loanRequests[_id].isAccepted == false, "Cannot cancel accepted loan request");
-    // Check that the current user is the borrower for the loan request
+    require(loanRequests[_id].isAccepted == false, "Cannot cancel an already accepted loan request");
+    //Check that the user cancelling the loan request is the borrower
     require(loanRequests[_id].borrower == msg.sender, "Only the borrower can cancel the loan request");
 
+    // delete the loan request from the mapping
     delete loanRequests[_id];
   }
 }
